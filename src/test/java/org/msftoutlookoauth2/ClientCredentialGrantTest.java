9a3416437cde3096b134453198db1a8bd5e21722
Tony Vo <ttrung.vo@gmail.com>
2022-11-08 04:45:33 -0700

    initial oauth2

---
+++ src/test/java/org/msftoutlookoauth2/ClientCredentialGrantTest.java
@@ -0,0 +1,14 @@
+package org.msftoutlookoauth2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ClientCredentialGrantTest {
+
+    @Test
+    public void foo() {
+        fail();
+    }
+
+}
